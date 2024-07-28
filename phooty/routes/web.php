8faa33fc6a0a2c3a57f88ec628b91d7cd3d6a9e3
ZammaBianca <zammatarosalvatore99@gmail.com>
2024-07-28 11:30:28 +0200

    demo project
    first navigation behavior
    
    # Conflicts:
    #	phooty/resources/views/about.blade.php
    #	phooty/resources/views/components/layout.blade.php
    #	phooty/resources/views/components/nav-link.blade.php
    #	phooty/resources/views/contact.blade.php
    #	phooty/resources/views/home.blade.php
    #	phooty/resources/views/welcome.blade.php

--- phooty/routes/web.php
+++ phooty/routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+// impostiamo una Route che si mette in ascolto delle GET request alla pagina '/', quando questa viene visitata esegue la function
 Route::get('/', function () {
-    return view('welcome');
+    return view('home'); // le views sono contenute nella cartella resources/views
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+  return view('contact');
+});
+
+Route::get('/foo', function () {
+    return ['foo' => 'bat']; //this will be parsed into json
 });
