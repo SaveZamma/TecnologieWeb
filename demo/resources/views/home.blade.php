898f6301b4db2d8f240075bf1e03f139d52192ee
ZammaBianca <zammatarosalvatore99@gmail.com>
2024-07-28 15:10:47 +0200

    demo project
    updated style using tailwingscss
    managed routing via component

--- demo/resources/views/home.blade.php
+++ demo/resources/views/home.blade.php
@@ -3,5 +3,9 @@
 {{--  anteponendo "x-" in modo che non venga confuso con tag preesistenti--}}
 
 <x-layout>
+  <x-slot:heading>
+    Home Page
+  </x-slot:heading>
+
   <h1>Hello from Home Page</h1>
 </x-layout>
