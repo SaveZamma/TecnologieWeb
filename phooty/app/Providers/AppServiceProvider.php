f04d5dad395d808dcb647bf4e3c2b2fb8cc9ce89
ZammaBianca <zammatarosalvatore99@gmail.com>
2024-08-06 15:27:13 +0200

    phooty project
    pagination
    php artisan vendor:publish

--- phooty/app/Providers/AppServiceProvider.php
+++ phooty/app/Providers/AppServiceProvider.php
@@ -3,23 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        Model::preventLazyLoading();
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    Model::preventLazyLoading();
+//    Paginator::defaultView('vendor.pagination.bootstrap-5');
+//    Paginator::useBootstrapFive();
+  }
 }
