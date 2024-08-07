a1916eab8569040975bb44460e9a3f19c6d25e80
ZammaBianca <zammatarosalvatore99@gmail.com>
2024-08-07 14:21:11 +0200

    phooty project
    form submission
    CSRF
    access request attributes
    fillable vs guarded fields for db tables

--- phooty/routes/web.php
+++ phooty/routes/web.php
@@ -9,20 +9,32 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-  $jobs = Job::with('employer')->simplePaginate(3);
-  return view('jobs', ['jobs' => $jobs]);
+  $jobs = Job::with('employer')->latest()->simplePaginate(3);
+  return view('jobs.index', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/create', function () {
+  return view('jobs.create');
 });
 
 // laravel capisce che i valori passati tra parentesi sono delle variabili e le passa direttamente alla funzione
 Route::get('/jobs/{id}', function ($id) {
   $job = Job::find($id);
-  return view('job', ['job' => $job]);
+  return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+  //...validation
+
+  Job::create([
+    'title' => request('title'),
+    'salary' => request('salary'),
+    'employer_id' => 1
+  ]);
+
+  return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
   return view('contact');
-});
-
-Route::get('/foo', function () {
-  return ['foo' => 'bat']; //this will be parsed into json
 });
