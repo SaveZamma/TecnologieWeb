1cc2a67756d08ea6dd0a1e0fb7ea08f85a382895
ZammaBianca <zammatarosalvatore99@gmail.com>
2024-08-11 09:37:28 +0200

    phooty project
    update and delete

--- phooty/routes/web.php
+++ phooty/routes/web.php
@@ -8,21 +8,25 @@ Route::get('/', function () {
   return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
   $jobs = Job::with('employer')->latest()->simplePaginate(3);
   return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
   return view('jobs.create');
 });
 
+// Show
 // laravel capisce che i valori passati tra parentesi sono delle variabili e le passa direttamente alla funzione
 Route::get('/jobs/{id}', function ($id) {
   $job = Job::find($id);
   return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
   //...validation
   request()->validate([
@@ -36,6 +40,47 @@ Route::post('/jobs', function () {
     'employer_id' => 1
   ]);
 
+  return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+  $job = Job::find($id);
+  return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+  // validate
+  request()->validate([
+    'title' => ['required', 'min:3'],
+    'salary' => ['required']
+  ]);
+
+  // authorize
+  // update the job
+  $job = Job::findOrFail($id); // to not manually fetch the job from the DB but let the framework working instead, see Laravel Route Model Binding
+  $job->title = request('title');
+  $job->salary = request('salary');
+  $job->save();
+
+  // persist
+  // the same as above
+//  $job->update([
+//    'title' => request('title'),
+//    'salary' => request('salary')
+//  ]);
+  // redirect to the job page
+  return redirect('/jobs/' . $job->id);
+});
+
+// Delete
+Route::delete('/jobs/{id}', function ($id) {
+  // authorize
+  // delete the job
+  $job = Job::findOrFail($id);
+  $job->delete();
+  // redirect
   return redirect('/jobs');
 });
 
